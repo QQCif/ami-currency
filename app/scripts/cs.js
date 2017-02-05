46fcaeeb61101b3ec8fa2788375fe115b0840c2d
wxx <superdavid2010@126.com>
2017-02-05 17:13:26 +0800

    Truncate function

--- app/scripts/cs.js
+++ app/scripts/cs.js
@@ -5,42 +5,31 @@ function currencyConvert(locale) {
   var currency_to = locale;
   // http://stackoverflow.com/questions/3139879/how-do-i-get-currency-exchange-rates-via-an-api-such-as-google-finance/16408368#16408368
   var yql_base_url = "https://query.yahooapis.com/v1/public/yql";
-  var yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+currency_from+currency_to+'")';
+  var yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("' + currency_from + currency_to + '")';
   var yql_query_url = yql_base_url + "?q=" + yql_query + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
-  var rate =0;
+  var rate = 0;
 
   // Fire Ajax using GET method
-  $.get( yql_query_url, function( data ) {
+  $.get(yql_query_url, function (data) {
     rate = data.query.results.rate.Rate;
 
     // Find price
-    if($(".off_price").length) {
-      var price = $(".price").contents().filter(function(){ return this.nodeType == 3; }).text();
-      //$(".price")[0].childNodes[1].nodeValue.trim();
-      console.log(price);
-      var localPrice = Math.trunc(trimPrice(price) * rate);
-      console.log(localPrice);
-      $(function () {
-        $(".price").fadeOut(200, function () {
-          $(this).text(localPrice + "\u00A0CNY").fadeIn(200);
-        });
+    var price = $(".price").contents().filter(function () { return this.nodeType == 3; }).text();
+    console.log(price);
+    var localPrice = Math.trunc(trimPrice(price) * rate);
+    console.log(localPrice);
+    $(function () {
+      $(".price").fadeOut(200, function () {
+        $(this).text(localPrice + "\u00A0CNY").fadeIn(200);
       });
-      //$(".price")[0].childNodes[1].nodeValue = localPrice + "\u00A0CNY";
-    } else {
-      var price = $(".price").text().trim();
-      var localPrice = Math.trunc(trimPrice(price) * rate);
-      $(function () {
-        $(".price").fadeOut(200, function () {
-          $(this).text(localPrice + "\u00A0CNY").fadeIn(200);
-        });
-      });
-    }
+    });
+
   });
 
   // Remove separator and JPY
   function trimPrice(string) {
-    return string.replace(/,/g,"").slice(0,-4);
+    return string.replace(/,/g, "").slice(0, -4);
   }
 }
 currencyConvert(localCurrencyCode);
